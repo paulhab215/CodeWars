bd436c7b204a74308d15f1f06a6952ff06780b3c
Paul Habjanetz <paulhab39@gmail.com>
2017-06-23 19:37:00 -0500

    Label Printer Replace
    Updated lable print to replace all correct letters with blank and get lengths

--- Label_Printer/label_printer.php
+++ Label_Printer/label_printer.php
@@ -1,15 +1,6 @@
 <?php # label_printer - return percent match
 
 function printerError($s) {
-  $numfound = 0;
-  $length=strlen($s);
-  for ($i=0; $i<$length; $i++) {  
-    if(ord($s[$i]) < 97 || ord($s[$i]) > 109){
-      $numfound += 1;
-    }     
-  }  
-  $final = $numfound."/".$length;
-  return $final;
+	return strlen(preg_replace('/a-m/', '', $s))."/".strlen($s);  //replace all correct letters with blank and get lengths
 }
-
 ?>
