56d7e4be2ca8e85c7e735134ef1ed843a7837c38
Paul Habjanetz <paulhab39@gmail.com>
2017-06-23 19:24:19 -0500

    strtr function
    Used strtr in order to get compliments by passing two key strings

--- DNA_Strand/dna_strand.php
+++ DNA_Strand/dna_strand.php
@@ -1,15 +1,5 @@
 <?php # dna strand return compliment array
-
 function DNA_strand($dna) {
-  $arraycheck = array('A','T','C','G');
-  $arrayoppos = array('T','A','G','C');  
-
-  $compliment = "";
-  for ($i=0; $i<strlen($dna); $i++) {
-    $key = array_search($dna[$i], $arraycheck); 
-    $compliment .= $arrayoppos[$key]; //capture key and use on opposite array.
-  }  
-  return $compliment;
+  return strtr($dna, 'ACGT', 'TGCA');
 }
-
 ?>
