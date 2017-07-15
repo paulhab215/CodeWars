d31bdffd3dd35593ceadf875e6812fc7fdf5a8fe
Paul Habjanetz <paulhab39@gmail.com>
2017-07-15 14:57:25 -0500

    Switched to array slice join

--- PHP/Consecutive_Strings/consecutive_strings.php
+++ PHP/Consecutive_Strings/consecutive_strings.php
@@ -1,18 +1,16 @@
 <?php # consecutive string - get $k consecutive longtest length string
 function longestConsec($strarr, $k) {
 
-$full = array();
-  for($i=0;$i<count($strarr);$i++){
-    $full[$i] = $strarr[$i];
-    for($j=1;$j<$k;$j++){
-      if(isset($strarr[$i+$j])){
-          $full[$i] .= $strarr[$i+$j];
-      }
-    }
-  }
-  
-  if($k <= 0 || $k > count($full)){return "";}
-  $lengths = array_map('strlen', $full);
-  return $full[array_search(max($lengths), $lengths)];
+	if($k <= 0 || $k > count($strarr)){return "";}
+
+	$full = array();
+  	foreach ($strarr as $key => $value) {
+   		$full[] = join('', array_slice($strarr, $key, $k));    
+  	}
+  	
+	$lengths = array_map('strlen', $full);
+  	return $full[array_search(max($lengths), $lengths)];
 }
 ?>
+
+
