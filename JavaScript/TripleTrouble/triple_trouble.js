2e5a263cdad3b580b8772fd3c511b8e1bbed1c52
Paul <paulhab39@gmail.com>
2017-08-29 11:51:09 -0500

    Upgraded

--- JavaScript/TripleTrouble/triple_trouble.js
+++ JavaScript/TripleTrouble/triple_trouble.js
@@ -1,5 +1,17 @@
 # return 1 if they first has three consec and second has 2 consec integers found in num 1
 
+//Best Practice Solution
+function tripledouble(num1, num2) {
+  for (let i = 0; i < 10; i++) {
+    if (new RegExp(`${i}{3}`).test(num1) && new RegExp(`${i}{2}`).test(num2)) {
+      return 1;
+    }
+  }
+  return 0;
+}
+
+
+// Original Solution
 function tripledouble(num1, num2) { 
   var arrtrip = (String(num1).match(/([0-9])\1\1/g) || []); // would return array of triples
   var arrdub  = (String(num2).match(/([0-9])\1/g) || []); // would return array of doubles
