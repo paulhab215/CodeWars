da0affb60ac48dbba410e475275d43e99041ef2d
Paul <paulhab39@gmail.com>
2017-08-29 12:56:40 -0500

    Utilized .map functions

--- JavaScript/WeirdStringCase/weird_string_case.js
+++ JavaScript/WeirdStringCase/weird_string_case.js
@@ -1,5 +1,17 @@
 # return string with every even index uppercase and every odd index lowercase
 
+//Best Practice Solution
+function toWeirdCase(string){
+  return string.split(' ').map(function(word){ //split each word into an array and map to function
+    return word.split('').map(function(letter, index){  //split each letter into an array and map to function
+      return index % 2 == 0 ? letter.toUpperCase() : letter.toLowerCase()  // if index even uppercase else lowercase
+    }).join('');
+  }).join(' ');
+}
+
+
+
+//Original Solution
 function toWeirdCase(str){
   str = str.split(' ');
     for(var i = 0; i < str.length; i++){
