ee57a068625f286fc423d3330a9ade8873c132a3
paulhab215 <paulhab39@gmail.com>
2017-09-02 20:04:15 -0500

    Revised solution
    Utilized .match

--- JavaScript/CountingDuplicates/counting_duplicates.js
+++ JavaScript/CountingDuplicates/counting_duplicates.js
@@ -1,4 +1,17 @@
 # return count of char count greater then 1
+
+//Best Practice Solutions
+function duplicateCount(text){
+  return text.toLowerCase().split('').filter(function(val, i, arr){
+    return arr.indexOf(val) !== i && arr.lastIndexOf(val) === i;
+  }).length;
+}
+
+function duplicateCount(text){
+  return (text.toLowerCase().split('').sort().join('').match(/([^])\1+/g) || []).length;
+}
+
+//Original solution
 function duplicateCount(text){
   var xx = 0; var freq = {};
   
