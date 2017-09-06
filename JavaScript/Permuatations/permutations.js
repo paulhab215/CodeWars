88e1a5eceae922e909ae90a9904a3ad6ea4dbb97
Paul Habjanetz <paulhab39@gmail.com>
2017-09-06 18:50:32 -0500

    Upgraded

--- JavaScript/Permuatations/permutations.js
+++ JavaScript/Permuatations/permutations.js
@@ -1,5 +1,21 @@
 # return all permuations of given string
 
+//Best Practice
+function permutations(string) {
+  var arr = string.split(''), tmp = arr.slice(), heads = [], out = [];
+  if(string.length == 1) return [string];
+  arr.forEach(function(v, i, arr) {
+    if(heads.indexOf(v) == -1) {
+      heads.push(v);
+      tmp.splice(tmp.indexOf(v), 1);
+      permutations(tmp.join('')).forEach(function(w) {out.push(v + w);});
+      tmp.push(v);
+    }
+  });
+  return out;
+}
+
+//Original Solution
 function permutations(string) {
   var final = [];
   
